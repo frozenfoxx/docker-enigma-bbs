285e5c4b41637f11e60d83ad4b9d8f5076eda5e5
FrozenFOXX <frozenfoxx@churchoffoxx.net>
2019-09-18 11:33:51 -0700

    Update enigma_config.sh

--- scripts/enigma_config.sh
+++ scripts/enigma_config.sh
@@ -1,9 +1,27 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-echo "Checking for an ENiGMA config file"
-if [ -f "/enigma-bbs/config/config.hjson" ]; then
+# Variables
+
+ENIGMAHOME=${ENIGMAHOME:-'/enigma-bbs'}
+
+# Logic
+
+echo "Checking for an ENiGMA config file..."
+
+if [[ -f "${ENIGMAHOME}/config/config.hjson" ]]; then
   echo " - Found config.hjson..."
 else
   echo " - No config file found, using basic config..."
-  cp /enigma-bbs/misc/config.hjson /enigma-bbs/config/config.hjson
+  cp ${ENIGMAHOME}/misc/config.hjson ${ENIGMAHOME}/config/config.hjson
 fi
+
+echo "Checking for theme files..."
+
+for themefile in menu prompt; do
+  if [[ -f "${ENIGMAHOME}/config/${themefile}.hjson" ]; then
+    echo " - Found ${themefile}.hjson"
+  else
+    echo " - ${themefile}.hjson not found, copying template..."
+    cp ${ENIGMAHOME}/misc/${themefile}_template.in.hjson ${ENIGMAHOME}/config/${themefile}.hjson
+  fi
+done
